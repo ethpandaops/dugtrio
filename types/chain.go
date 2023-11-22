47f85b218c56a58fc1c33159eed2a0677dea1eff
pk910 <philipp@pk910.de>
2023-11-22 19:12:24 +0100

    make specs check less strict

--- types/chain.go
+++ types/chain.go
@@ -19,15 +19,12 @@ type ChainConfig struct {
 	ConfigName           string         `yaml:"CONFIG_NAME"`
 	MinGenesisTime       time.Time      `yaml:"MIN_GENESIS_TIME"`
 	GenesisForkVersion   phase0.Version `yaml:"GENESIS_FORK_VERSION"`
-	GenesisDelay         time.Duration  `yaml:"GENESIS_DELAY"`
 	AltairForkVersion    phase0.Version `yaml:"ALTAIR_FORK_VERSION"`
 	AltairForkEpoch      uint64         `yaml:"ALTAIR_FORK_EPOCH"`
 	BellatrixForkVersion phase0.Version `yaml:"BELLATRIX_FORK_VERSION"`
 	BellatrixForkEpoch   uint64         `yaml:"BELLATRIX_FORK_EPOCH"`
 	CappellaForkVersion  phase0.Version `yaml:"CAPELLA_FORK_VERSION"`
 	CappellaForkEpoch    uint64         `yaml:"CAPELLA_FORK_EPOCH"`
-	DenebForkVersion     phase0.Version `yaml:"DENEB_FORK_VERSION"`
-	DenebForkEpoch       uint64         `yaml:"DENEB_FORK_EPOCH"`
 	SecondsPerSlot       time.Duration  `yaml:"SECONDS_PER_SLOT"`
 	SlotsPerEpoch        uint64         `yaml:"SLOTS_PER_EPOCH"`
 }
